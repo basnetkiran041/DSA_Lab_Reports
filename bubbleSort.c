7321120113b94c37743658a5dc1d0be837ea3703
Kiran Basnet <basnetkiran041@gmail.com>
2025-06-07 23:33:06 +0545

    Bubble_sort

---
+++ bubbleSort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;  // optimization: check if any swap occurred
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        // If no swaps, array is already sorted
+        if (!swapped)
+            break;
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Main function
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    bubbleSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
